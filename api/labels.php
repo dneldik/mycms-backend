d2d25001fe64007accfedb00d281bed14c15cc14
dneldik <dneldik@gmail.com>
2021-04-10 14:34:10 +0200

    Add the public getUrl Parameters() method to Request.php
    Add a private iterate() method to Request.php
    Add a private getSafeString() method to Request.php

--- api/labels.php
+++ api/labels.php
@@ -9,11 +9,19 @@ $root = $_SERVER["DOCUMENT_ROOT"];
 require_once "{$root}/src/Request.php";
 require_once "{$root}/src/HttpHeaders.php";
 
+$Request = new Request($_SERVER);
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$Request = new Request($requestUri);
+// Temporary code for debuging.
+echo '<pre>';
+print_r($Request->getUrlParameters());
+echo '</pre>';
 
 // Todo:
 // Handle data that will be send back as response.
 
 (new HttpHeaders())->setHeaders();
+
+// Temporary code to view the value of the SERVER array.
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
--- src/Request.php
+++ src/Request.php
@@ -6,13 +6,72 @@ namespace Api;
 
 class Request
 {
-  private $uri;
+  private array $server;
 
-  public function __construct(string $uri)
+  public function __construct(array $server)
   {
-    $this->uri = $uri;
+    $this->server = $server;
   }
 
-  // Todo:
-  // handle request uri
+  /**
+  * Read parameter from server['QUERY_STRING'].
+  * Returns parameters after sanitization as an array.
+  */
+  public function getUrlParameters(): array
+  {
+    $safeUrlParams = [];
+    if(isset($this->server['QUERY_STRING'])) {
+      parse_str($this->server['QUERY_STRING'], $urlParams);
+      $safeUrlParams = $this->iterate($urlParams, array($this, 'getSafeString'));
+    }
+    return $safeUrlParams;
+  }
+
+  /**
+  * Iterates over an $array given as parameter.
+  * Returns new array. Every key and value of new array
+  * is returned by $callback function.
+  * $callbac function must return string.
+  */
+  private function iterate(array $array, callable $callback): array
+  {
+    $finalArray = [];
+
+    if(is_array($array)) {
+
+      foreach ($array as $key => $value) {
+
+        if(is_array($value)) {
+          $value = $this->iterate($value, $callback);
+        }
+
+        $safeKey = $callback($key);
+
+        if(!is_array($value)) {
+          $safeValue = $callback($value);
+          $finalArray[$safeKey] = $safeValue;
+        } else {
+          $finalArray[$safeKey] = $value;
+        }
+      }
+    }
+    return $finalArray;
+  }
+
+  /**
+  * Sanitizes $string given as parameter.
+  * Returns sanitized string.
+  * Removes HTML and PHP tags.
+  * Sanitizes string.
+  * Removes special characters.
+  */
+  private function getSafeString(string $string): string
+  {
+    $finalString = $string;
+    $finalString = strip_tags($finalString);
+    $finalString = filter_var($finalString, FILTER_SANITIZE_STRING);
+    $finalString = preg_replace('/[^A-Za-z0-9\-]/', '', $finalString);
+
+    return $finalString;
+  }
 }
