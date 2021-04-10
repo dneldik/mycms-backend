95b6118188d44b2485855ecf511be236f7ca6327
dneldik <dneldik@gmail.com>
2021-04-10 15:03:01 +0200

    Add debug.php with the dump() function.

--- api/labels.php
+++ api/labels.php
@@ -9,6 +9,10 @@ $root = $_SERVER["DOCUMENT_ROOT"];
 require_once "{$root}/src/Request.php";
 require_once "{$root}/src/HttpHeaders.php";
 
+// Function in debug.php - development only.
+// Function name: dump. Prints the passed value.
+require_once "{$root}/src/utils/debug.php";
+
 $Request = new Request($_SERVER);
 
 // Temporary code for debuging.
